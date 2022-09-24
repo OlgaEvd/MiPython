2a5d994bc6261ad584e7b2bef3e89b32fb1a087f
«Olga» <elfica@yandex.ru>
2022-09-25 01:17:17 +0800

    Перемешивание списка метод21

--- HW2-5.py
+++ HW2-5.py
@@ -1,16 +1,24 @@
 #Метод № 1: алгоритм перетасовки Фишера–Йейтса
 
  
-import random
+# import random
  
-test_list = [1, 4, 5, 6, 3]
-print ("Исходный список: " + str(test_list))
+# test_list = [1, 4, 5, 6, 3]
+# print ("Исходный список: " + str(test_list))
  
-# алгоритм перетасовки Фишера–Йейтса
-for i in range(len(test_list)-1, 0, -1):
+# # алгоритм перетасовки Фишера–Йейтса
+# for i in range(len(test_list)-1, 0, -1):
        
-    j = random.randint(0, i + 1)
+#     j = random.randint(0, i + 1)
       
-    test_list[i], test_list[j] = test_list[j], test_list[i]
+#     test_list[i], test_list[j] = test_list[j], test_list[i]
      
-print ("Перемешанный список : " +  str(test_list))
+# print ("Перемешанный список : " +  str(test_list))
+
+# Метод № 2: использование random.shuffle()
+ 
+import random
+test_list = [1, 2, 3, 4, 5]
+print ("The original list is : " + str(test_list))
+random.shuffle(test_list)
+print ("The shuffled list is : " +  str(test_list))
